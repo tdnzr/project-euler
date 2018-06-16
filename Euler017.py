4d8410f808d0c621486337bbe0132d87550fb172
MondSemmel <MondSemmel@gmail.com>
2018-06-16 18:28:22 +0200

    Solution 17 improved: It's been made cleaner and more readable.

--- Euler017.py
+++ Euler017.py
@@ -1,42 +1,37 @@
 # Solution to Project Euler problem 17.
 
 # I don't see how to calculate this trivially, so I'll do it via logical thinking,
-# tons of ugly special cases, and the automated construction of the 1000 strings for the 1000 numbers.
-# The result is a mess. There are just too many special cases in languages. At least the code works!
+# several ugly special cases, and the automated construction of the 1000 strings for the first 1000 numbers.
+# The result is pretty messy - there are just too many special cases in languages - but it used to be much worse...
 
 
 def run():
     oneDigit = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
-    specialTen = ["ten"]
-    specialNumbers = ["eleven", "twelve", "thirteen", "fourteen", "fifteen",
-                        "sixteen", "seventeen", "eighteen", "nineteen"]
-    twoDigitsOld = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
-    specialThousand = ["onethousand"] # Spaces don't count.
-    twoDigits = []
-    threeDigits = []
+    twoDigitsRegular = ["twenty", "thirty", "forty",
+                        "fifty", "sixty", "seventy", "eighty", "ninety"]
+
+    # Only the first 99 numbers are full of special cases. Once we've constructed these, the rest is straightforward.
+    one_to_ninetynine = []
+    one_to_ninetynine.extend(oneDigit)
+    one_to_ninetynine.extend(["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+                              "sixteen", "seventeen", "eighteen", "nineteen"])
+
+    for i in twoDigitsRegular:
+        one_to_ninetynine.append(i)
+        for j in oneDigit:
+            one_to_ninetynine.append(i + j)  # Hyphens omitted.
+
+    # Put 1...99 into the final list:
     allDigits = []
+    allDigits.extend(one_to_ninetynine)
 
-    for i in twoDigitsOld:
-        twoDigits.append(i)
-        for j in oneDigit:
-            twoDigits.append(i + j)  # i + "-" + j or "%s-%s" would be correct, but there's no need to count hyphens.
-
+    # Now that we've constructed the first 99 numbers, constructing numbers 100...999, and 1000, is easy:
     for i in oneDigit:
-        threeDigits.append(i + "hundred")
-        for j in oneDigit:
-            threeDigits.append(i + "hundredand" + j)  # Spaces omitted.
-        threeDigits.append(i + "hundredandten")  # Spaces omitted.
-        for j in specialNumbers:
-            threeDigits.append(i + "hundredand" + j)  # Spaces omitted.
-        for j in twoDigits:
-            threeDigits.append(i + "hundredand" + j)  # Spaces omitted.
+        allDigits.append(i + "hundred")
+        for j in one_to_ninetynine:
+            allDigits.append(i + "hundredand" + j)  # Spaces omitted.
 
-    allDigits.extend(oneDigit)
-    allDigits.extend(specialTen)
-    allDigits.extend(specialNumbers)
-    allDigits.extend(twoDigits)
-    allDigits.extend(threeDigits)
-    allDigits.extend(specialThousand)
+    allDigits.append("onethousand")  # Spaces omitted.
 
     return sum([len(s) for s in allDigits])
 
