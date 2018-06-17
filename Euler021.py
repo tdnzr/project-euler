911dbcc5830c1d3e5cd3621266ecc0a3a639e90b
MondSemmel <MondSemmel@gmail.com>
2018-06-17 18:15:03 +0200

    Solution to problem 21, plus test.

---
+++ Euler021.py
@@ -0,0 +1,18 @@
+# Solution to Project Euler problem 21.
+
+from euler_toolbox import proper_divisor_sum
+
+
+def run():
+    amicable_sum = 0
+    for numOne in range(10000):  # Amicable numbers under 10000.
+        numTwo = proper_divisor_sum(numOne)
+        # To avoid double-counting, we require numOne <= numTwo.
+        # And numOne == numTwo is explicitly ruled out in the exercise.
+        if numOne < numTwo and numOne == proper_divisor_sum(numTwo):
+            amicable_sum += numOne + numTwo
+    return amicable_sum
+
+
+if __name__ == "__main__":
+    print(run())
