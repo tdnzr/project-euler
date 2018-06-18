cceaae82559703187a45f11e955a4d6861a5eebe
MondSemmel <MondSemmel@gmail.com>
2018-06-18 12:01:43 +0200

    Solution to problem 10, plus tests.

---
+++ Euler010.py
@@ -0,0 +1,15 @@
+# Solution to Project Euler problem 10.
+
+
+from euler_toolbox import prime_sieve
+
+
+def run():
+    limit = 2_000_000  # Numbers *below* 2,000,000.
+    sieve = prime_sieve(limit)
+    prime_sum = sum(n for n in range(limit) if not sieve[n])
+    return prime_sum
+
+
+if __name__ == "__main__":
+    print(f"The sum of all the primes below 2000000 is {run()}")
