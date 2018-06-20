9f9c994d0a71327921a5fc6224b7ca443c6d634e
MondSemmel <MondSemmel@gmail.com>
2018-06-20 14:31:48 +0200

    Solution to problem 40, plus test.

---
+++ Euler040.py
@@ -0,0 +1,26 @@
+# Solution to Project Euler problem 40.
+
+# Straightforward solution: Just concatenate the integers
+# until I reach a string with a million digits, then
+# compute the product in question.
+
+
+def run():
+    # Concatenate the integers into a string:
+    testString = ""
+    i = 0
+    while len(testString) < 1_000_000:
+        i += 1
+        testString += str(i)
+
+    # Then calculate the product of the digits at position
+    # 1, 10, 100, ..., 1,000,000:
+    product = 1
+    for i in range(7):
+        product *= int(testString[(10**i) - 1])
+
+    return product
+
+
+if __name__ == "__main__":
+    print(run())
