3f74612f1335574c4233984802610d0ff82fb2d1
MondSemmel <MondSemmel@gmail.com>
2018-06-22 13:40:31 +0200

    Improved solution 26: - fixed the string result of long division - it now also works in case of dividend >= divisor - simplified the algorithm

--- Euler026.py
+++ Euler026.py
@@ -2,40 +2,59 @@
 
 # I struggled a lot with this problem and had to look up some stuff.
 # Solution: Do long division until we end up dividing by the same number again.
-# Note that my implementation of long division here only works properly if dividend <= divisor.
-# It also computes the string of the remainder, which is neat but not required in the exercise.
+# Note that my solution also returns the correct result of the division as a string
+# (including the recurring part).
+# While kind of neat - I certainly learned something by implementing it -,
+# it's not actually required in the exercise.
+
 
 def long_division(dividend, divisor):
-    """Divides the dividend by the divisor via long division. This implementation requires dividend <= divisor.
+    """Divides the dividend by the divisor via long division.
     Returns the result as a string, plus the length of the recurring part of the remainder."""
-    
-    assert dividend <= divisor, "This implementation of long division only works properly if dividend <= divisor."
     
     remainderString = ""
     dividends_list = []
     recurringLength = 0
+
+    # Compute the part to the left of the period first,
+    # e.g. 10 / 4 = 2.5, so we compute the "2." part first:
+    quotient, remainder = divmod(dividend, divisor)  # in case of integers, divmod(a, b) = (a // b, a % b)
+    remainderString += str(quotient)
+    if remainder == 0:
+        return remainderString, 0
+    # Then if there's a remainder, we add a period in preparation for long division
+    # before we head into the while-loop:
+    else:
+        remainderString += "."
+        dividend = remainder * 10
+    
+    # Now that dividend < divisor, we can deal with long division without worrying about the period:
     while dividend not in dividends_list:
         dividends_list.append(dividend)
-        if dividend % divisor == 0:
-            remainderString += (str(dividend // divisor))
-            return remainderString[0] + "." + remainderString[1:], 0
-        elif dividend > divisor:
-            remainderString += (str(dividend // divisor))
-            dividend = (dividend % divisor) * 10
-        else:  # dividend < divisor
-            remainderString += "0"
-            dividend = (dividend % divisor) * 10
+        # Compute quotient and remainder, and append the quotient to the remainderString.
+        # Then if the remainder is zero, we're done; otherwise, just like in division on paper,
+        # we multiply the remainder by ten and begin the process anew.
+        quotient, remainder = divmod(dividend, divisor)
+        remainderString += str(quotient)
+        if remainder == 0:
+            return remainderString, 0
+        else:
+            dividend = remainder * 10
+
     recurringLength = len(dividends_list) - dividends_list.index(dividend)
-    return remainderString[0] + "." + remainderString[1:], recurringLength
+    # Put the recurring part of the remainder in parentheses,
+    # for no other reason than because we can:
+    remainderString = remainderString[:-recurringLength] + "(" + remainderString[-recurringLength:] + ")"
+    return remainderString, recurringLength
 
 
 def run():
     maxDigit = 0
     maxRecurring = 0
-    for i in range(1, 1000):  # 1/d with d < 1000
+    for i in range(1, 1000):  # 1 / d with d < 1000
         remainderString, recurring = long_division(1, i)
-        #if i < 200:
-        #	print(f"1/{i:2} = {remainderString} --- {recurring}")
+        # Optional, for debugging:
+        # print(f"1 / {i:4} = {remainderString} --- {recurring}")
         if maxRecurring < recurring:
             maxRecurring = recurring
             maxDigit = i
